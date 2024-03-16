71145e37c18348c2b21777f772a12f6e7165c6a1
Aaron Graves <ajgraves@ajgraves.com>
2024-03-16 14:51:55 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Aaron Graves on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
